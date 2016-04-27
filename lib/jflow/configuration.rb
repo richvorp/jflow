8de66e9f231f01b379210a2ce04d70f4569bc167
Christophe Verbinnen <christophe.verbinnen@lookout.com>
2016-04-27 11:07:08 -0700

    Add custom SWF metrics

--- lib/jflow/configuration.rb
+++ lib/jflow/configuration.rb
@@ -1,13 +1,14 @@
 module JFlow
   class Configuration
 
-    attr_accessor :swf_client, :load_paths, :logger, :activity_map
+    attr_accessor :swf_client, :load_paths, :logger, :activity_map, :cloudwatch_client
 
     def initialize
-      @swf_client    = nil
-      @load_paths    = []
-      @logger        = Logger.new(STDOUT)
-      @activity_map  = JFlow::Activity::Map.new
+      @swf_client        = nil
+      @cloudwatch_client = nil
+      @load_paths        = []
+      @logger            = Logger.new(STDOUT)
+      @activity_map      = JFlow::Activity::Map.new
     end
   end
 end
